cfe3d99ae171b4cffdca7ec9df39ce3095615969
Apokatastasis <ramirolonatti@gmail.com>
2021-12-10 00:46:14 -0300

    added playRound function

--- script.js
+++ script.js
@@ -1,4 +1,48 @@
+    let computerScore = 0;
+    let playerScore = 0;
+    let playerSelection = playerChoice();
+    let computerSelection = computerPlay();
+    
+    function game (){
+
+        for(let i = 1; i<=5;i++){
+            computerPlay();
+            playerChoice();
+            playRound(playerSelection, computerSelection);
+        }
+        
+    }
+    
+
+    
     function playRound(playerSelection, computerSelection) {
+
+        let roundResult;
+
+        switch(playerSelection){
+            case "rock":
+                if(computerSelection==="rock") roundResult = "It's a tie";
+                if(computerSelection==="paper") roundResult = "You lost";
+                if(computerSelection==="scissors") roundResult = "You win";
+                break;
+            case "paper":
+                if(computerSelection==="rock") roundResult = "You win";
+                if(computerSelection==="paper") roundResult = "It's a tie";
+                if(computerSelection==="scissors") roundResult = "You lost";
+                break;
+            case "scissors":
+                if(computerSelection==="rock") roundResult = "You lost";
+                if(computerSelection==="paper") roundResult = "You win";
+                if(computerSelection==="scissors") roundResult = "It's a tie";
+                break;
+ 
+        }
+
+        return roundResult;
+
+        
+        
+
         // your code here!
         //evaluates who wins
     }
@@ -28,8 +72,7 @@
 
     }
 
-    let playerSelection = playerChoice();
-    let computerSelection = computerPlay();
+    
     //console.log(playRound(playerSelection, computerSelection));
 
   
