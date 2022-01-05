975c814a6cc723a912350a4de030270515a63dac
Apokatastasis <ramirolonatti@gmail.com>
2022-01-05 17:27:48 -0300

    UI for the game

--- script.js
+++ script.js
@@ -1,49 +1,138 @@
+
     let computerScore = 0;
     let playerScore = 0;
     let playerSelection;
     let computerSelection;
     let roundResult;
-
-    game();
+//Selected elemntets for DOM manipulation
+    let game = document.querySelector('.game');
+    let rock = document.querySelector('.rock');
+    let paper = document.querySelector('.paper');
+    let scissors = document.querySelector('.scissors');
+    let userChoice = document.querySelector('.user-choice');
+    let computerChoice = document.querySelector('.computer-choice');
+    let boardRoundResult = document.querySelector('.round-result');
+    let boardMessage = document.querySelector('.board-message');
+    let scorePlayer = document.querySelector('.score-user');
+    let scoreComputer = document.querySelector('.score-computer');
+    let modalResult = document.querySelector('.modal-result');
+    let replayBtn = document.querySelector('.replay-btn');
+    let modal = document.getElementById("myModal");
+    let creator = document.querySelector('.apokatastasis');
+    let replaySound = document.querySelector("#replay-sound");
+    let intro = document.querySelector('.intro');
+    let startButton = document.getElementById('start-button');
+// Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
     
-    function game (){
 
-        for(let i = 1; i<=5;i++){
-            playerSelection = playerChoice();
-            computerSelection = computerPlay();
-            roundResult = playRound(playerSelection, computerSelection);
-            console.log(`Round: ${i} Computer: ${computerSelection}  Player: ${playerSelection} Round result: ${roundResult}`);
 
-        }
-        score(playerScore, computerScore);
+//  close the modal
+    span.onclick = function() {
+    replaySound.play();
+    modal.style.display = "none";
+    game.style.display = "none";
+    creator.style.display = "flex";
+        
+    }    
+
+
+
+    rock.addEventListener('click', ()=> handleClick('rock'));
+    paper.addEventListener('click', ()=> handleClick('paper'));
+    scissors.addEventListener('click', ()=> handleClick('scissors'));
+    replayBtn.addEventListener('click', replay);
+    startButton.addEventListener('click', startGame);
+
+    function startGame(){
+        replaySound.play();
+        game.style.display= 'flex';
+        intro.style.display = 'none';
+    }
+
+    function replay(){
+        
+        
+        replaySound.play();
+
+        playerScore = 0;
+        computerScore = 0;
+        scorePlayer.textContent = 0;
+        scoreComputer.textContent = 0;
+        boardMessage.textContent = "";
+        boardMessage.textContent = 'First to score 5 points wins!'
+        boardRoundResult.textContent = "Choose your weapon!";
+        modal.style.display = "none";
+        userChoice.src = "./img/question-mark.png";
+        computerChoice.src = "./img/question-mark.png";
+    }
+
+    function handleClick(selection){
+        playSound();
+        playerSelection = selection;
+        computerSelection = computerPlay();
+        swapChoice(selection);
+        
+        roundResult = playRound(playerSelection, computerSelection);
+        boardRoundResult.textContent = roundResult;
+        scorePlayer.textContent = playerScore;
+        scoreComputer.textContent = computerScore;
+        
+        isGameOver();
+        
+        
+
+    }
+
+    function isGameOver(){
+        if (playerScore===5 || computerScore ===5) {
+
+            modal.style.display = "block";
+            score();
+                       
+        };
         
     }
 
-    function score(playerScore, computerScore){
-
-        (playerScore>computerScore)? console.log(`You won the game \nPLAYER: ${playerScore} COMPUTER: ${computerScore}`) :
-        (playerScore<computerScore)? console.log(`You lost the game \nPLAYER: ${playerScore} COMPUTER: ${computerScore}`) : 
-        console.log(`It's a tie \nPLAYER: ${playerScore} COMPUTER: ${computerScore}`);
 
 
+
+    //updates the image with the selected weapon
+    function swapChoice(weapon){
+            
+            (weapon=='rock')? userChoice.src = "./img/rock.png":
+            (weapon=='paper')? userChoice.src = "./img/paper.png":
+            userChoice.src = "./img/scissors.png";
+
+            (computerSelection=='rock')? computerChoice.src = "./img/rock.png":
+            (computerSelection=='paper')? computerChoice.src = "./img/paper.png":
+            computerChoice.src = "./img/scissors.png";
+
+
+              
     }
-    
+   
 
-    
+   
     function playRound(playerSelection, computerSelection) {
 
         let roundResult;
 
         switch(playerSelection){
             case "rock":
-                if(computerSelection==="rock") roundResult = "It's a tie";
+                if(computerSelection==="rock") {
+                    roundResult = "It's a tie";
+                    boardMessage.textContent = 'try again!';    
+                }
                 if(computerSelection==="paper") {
                     roundResult = "You lost";
+                    boardMessage.textContent = 'Paper beats rock!';
                     computerScore++;
 
                 }
                 if(computerSelection==="scissors"){
                      roundResult = "You win";
+                     boardMessage.textContent = 'Rock beats scissors!';
                      playerScore++;
                 }
                 break;
@@ -51,35 +140,42 @@
             case "paper":
                 if(computerSelection==="rock") {
                      roundResult = "You win";
+                     boardMessage.textContent = 'Paper beats rock!';
                      playerScore++;
                 }
-                if(computerSelection==="paper") roundResult = "It's a tie";
+                if(computerSelection==="paper") {
+
+                    roundResult = "It's a tie";
+                    boardMessage.textContent = 'try again!';
+                }
                 if(computerSelection==="scissors"){ 
                     roundResult = "You lost";
+                    boardMessage.textContent = 'Scissors beats paper!';
                     computerScore++;
                 }
                 break;
             case "scissors":
                 if(computerSelection==="rock") {
                      roundResult = "You lost";
+                     boardMessage.textContent = 'Rock beats scissors!'
                      computerScore++;
                 }
                 if(computerSelection==="paper"){
                      roundResult = "You win";
+                     boardMessage.textContent = 'Scissors beats paper!'
                      playerScore++;
                 }
-                if(computerSelection==="scissors") roundResult = "It's a tie";
+                if(computerSelection==="scissors") {
+                    roundResult = "It's a tie";
+                    boardMessage.textContent = 'try again!'
+                }
                 break;
  
         }
 
         return roundResult;
 
-        
-        
 
-        // your code here!
-        //evaluates who wins
     }
 
     function computerPlay(){
@@ -89,27 +185,22 @@
         return weapons[randomIndex];
 
     }
-  
-    function playerChoice(){
 
-    let choice;  
+    function score(){
 
-    do{
-
-        choice = prompt("Choose rock, paper or scissors.").toLowerCase();
-
-        if(choice !== "paper" && choice !== "rock" && choice !== "scissors") alert("Please enter a valid option.");
-
-    }while(choice !== "paper" && choice !== "rock" && choice !== "scissors");
-
-    return choice;
-
+        if(playerScore>computerScore){
+            modalResult.textContent = "You won the game!";
+        } else{
+            modalResult.textContent = "You lost the game! Try again";
+        }
+        
 
     }
 
+    function playSound(){
+        const clickSound = document.querySelector("#click-sound")
+        clickSound.play();
+    }
+
+
     
-    //console.log(playRound(playerSelection, computerSelection));
-
-  
-
-   
